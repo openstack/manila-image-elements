9637c86877c91c04ff39236acafcfa43b1d22c36
Ben Swartzlander <ben@swartzlander.org>
2016-03-02 16:41:17 -0500

    Don't run bashate on conf files
    Bashate seems to be running on many files it shouldn't. There
    is an error related to a conf file, so I'm explicitly excluding
    conf files. Also reformat the run_bashate script so it doesn't
    warn about itself.
    
    Change-Id: I3986ecefcfb91c6a090697c782371dd100dc0d67

--- tools/run_bashate.sh
+++ tools/run_bashate.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -xe
 
-find ./ -not -wholename \*.tox/\* -and \( -name \*.sh -or -wholename \*.d/\* -and -not -name \*.md -and -not -name \*.rst -and -not -name \*.py \) -print0 | xargs -0 bashate -v
+find ./ -not -wholename \*.tox/\* -and \
+    \( -name \*.sh -or -wholename \*.d/\* -and \
+    -not -name \*.md -and -not -name \*.rst -and \
+    -not -name \*.py  -and -not -name \*.conf \) \
+    -print0 | xargs -0 bashate -v
